15249835de42e3c4efe71d0c9eba4877a4444cb8
KelvinSeamount <kelvinnwachukwu89@gmail.com>
2023-06-01 21:33:27 +0200

    refactor script code

--- js/app.js
+++ js/app.js
@@ -1,31 +1,36 @@
 const conTrol = document.querySelector(".form-control");
-const cardEl = document.querySelector(".card-text");
-const cardContainer = document.querySelector("card-container");
+const cardContainer = document.querySelector(".card-container");
 
-conTrol.addEventListener("change", function () {
-  cardEl.innerHTML = "Loading Please Wait...";
-  search = cardEl.value;
+conTrol.addEventListener("change", function (event) {
+  console.log(event);
+  //This clears the container displaying the html content
+  cardContainer.innerHTML = "";
+  search = event.target.value;
 
   fetch(`https://dummyjson.com/products/category/${search}`, {
     method: "GET",
   })
     .then(function (data) {
+      console.log(data);
       return data.json();
     })
-    .then(function (response) {
-      response.forEach(function (array) {
+    .then(function (fufilled) {
+      fufilled.products.forEach(function (array, index) {
+        console.log(cardContainer, "hello");
         const html = `
-           <div class="card">
+            <div class="card">
               <img src="${array.images[0]}" alt="img" />
               <p><span>Brand :</span></p>
-              <p><span>Category :</span></p>
+              <p><span>Category :</span>${array.category}</p>
               <p><span>Description :</span></p>
               <p><span>Price :</span></p>
               <p><span>Rating :</span></p>
               <p><span>Discount Percentage :</span></p>
               <p><span>In Stock :</span></p>
-           </div>
+            </div>
         `;
+        cardContainer.insertAdjacentHTML("beforeend", html);
+        cardContainer.style.opacity = 1;
       });
     });
 });

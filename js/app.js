680b859581a659842fafa6de58927e3eb8e91146
KelvinSeamount <kelvinnwachukwu89@gmail.com>
2023-06-03 01:36:04 +0200

    change on script

--- js/app.js
+++ js/app.js
@@ -20,13 +20,13 @@ conTrol.addEventListener("change", function (event) {
         const html = `
             <div class="card">
               <img src="${array.images[0]}" alt="img" />
-              <p><span>Brand :</span></p>
-              <p><span>Category :</span>${array.category}</p>
-              <p><span>Description :</span></p>
-              <p><span>Price :</span></p>
-              <p><span>Rating :</span></p>
-              <p><span>Discount Percentage :</span></p>
-              <p><span>In Stock :</span></p>
+              <p><span>Brand : </span>${array.brand}</p>
+              <p><span>Category : </span>${array.category}</p>
+              <p><span>Description : </span>${array.description}</p>
+              <p><span>Price : </span>${array.price}</p>
+              <p><span>Rating : </span>${array.rating}</p>
+              <p><span>Discount Percentage : </span> ${array.discountPercentage}</p>
+              <p><span>In Stock : </span>${array.stock}</p>
             </div>
         `;
         cardContainer.insertAdjacentHTML("beforeend", html);

ad5b5e3bff868d968265b552056463e45da5ddd5
KelvinSeamount <kelvinnwachukwu89@gmail.com>
2023-06-06 19:38:48 +0200

    object destruct

--- js/app.js
+++ js/app.js
@@ -16,17 +16,26 @@ conTrol.addEventListener("change", function (event) {
     })
     .then(function (fufilled) {
       fufilled.products.forEach(function (array, index) {
-        console.log(cardContainer, "hello");
+        //Destructing
+        const {
+          brand,
+          category,
+          description,
+          price,
+          rating,
+          discountPercentage,
+          stock,
+        } = array;
         const html = `
             <div class="card">
               <img src="${array.images[0]}" alt="img" />
-              <p><span>Brand : </span>${array.brand}</p>
-              <p><span>Category : </span>${array.category}</p>
-              <p><span>Description : </span>${array.description}</p>
-              <p><span>Price : </span>${array.price}</p>
-              <p><span>Rating : </span>${array.rating}</p>
-              <p><span>Discount Percentage : </span> ${array.discountPercentage}</p>
-              <p><span>In Stock : </span>${array.stock}</p>
+              <p><span>Brand : </span>${brand}</p>
+              <p><span>Category : </span>${category}</p>
+              <p><span>Description : </span>${description}</p>
+              <p><span>Price : </span>${price}</p>
+              <p><span>Rating : </span>${rating}</p>
+              <p><span>Discount Percentage : </span> ${discountPercentage}</p>
+              <p><span>In Stock : </span>${stock}</p>
             </div>
         `;
         cardContainer.insertAdjacentHTML("beforeend", html);

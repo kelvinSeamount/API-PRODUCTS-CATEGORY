dbae3acaba892211067ec3402b73ce7f955228e7
KelvinSeamount <kelvinnwachukwu89@gmail.com>
2023-07-27 14:01:09 +0200

    change to aysnc function

--- js/app.js
+++ js/app.js
@@ -1,32 +1,31 @@
 const conTrol = document.querySelector(".form-control");
 const cardContainer = document.querySelector(".card-container");
 
-conTrol.addEventListener("change", function (event) {
+conTrol.addEventListener("change", async function (event) {
   console.log(event);
   //This clears the container displaying the html content
   cardContainer.innerHTML = "";
   search = event.target.value;
 
-  fetch(`https://dummyjson.com/products/category/${search}`, {
-    method: "GET",
-  })
-    .then(function (data) {
-      console.log(data);
-      return data.json();
-    })
-    .then(function (fufilled) {
-      fufilled.products.forEach(function (array, index) {
-        //Destructing
-        const {
-          brand,
-          category,
-          description,
-          price,
-          rating,
-          discountPercentage,
-          stock,
-        } = array;
-        const html = `
+  const response = await fetch(
+    `https://dummyjson.com/products/category/${search}`,
+    {
+      method: "GET",
+    }
+  );
+  const responseData = await response.json();
+  responseData.products.forEach(function (array, index) {
+    //Destructing
+    const {
+      brand,
+      category,
+      description,
+      price,
+      rating,
+      discountPercentage,
+      stock,
+    } = array;
+    const html = `
             <div class="card">
               <img src="${array.images[0]}" alt="img" />
               <p><span>Brand : </span>${brand}</p>
@@ -38,8 +37,7 @@ conTrol.addEventListener("change", function (event) {
               <p><span>In Stock : </span>${stock}</p>
             </div>
         `;
-        cardContainer.insertAdjacentHTML("beforeend", html);
-        cardContainer.style.opacity = 1;
-      });
-    });
+    cardContainer.insertAdjacentHTML("beforeend", html);
+    cardContainer.style.opacity = 1;
+  });
 });
